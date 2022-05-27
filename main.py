ed422c5f1afb5584a62126c387c8158b65fab8b0
Renat Kalimulin <pianoparrot93@gmail.com>
2022-05-27 04:06:21 +0300

    fixed some text

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ class VkDownloader:
         :param google_token: token for google drive
         """
         if not any([ya_token, google_token]):
-            raise Exception('yandex token or google token should be entered')
+            raise Exception('yandex token or google token should be entered.')
         self.profile_id = profile_id
-        self.data_file = {'photos': []}  # future json file
+        self.data_file = {'photos': []}  # future .json file
         self.ya_token = str(ya_token)
         self.google_token = str(google_token)
 
         with open('vk_token.txt', 'r') as token:
-            self.vk_token = str(token.read())  # getting token from file 'vk_token.txt'
+            self.vk_token = str(token.read())  # gets token from the file 'vk_token.txt'
         self.respond = None
 
     def upload_profile_photos_to_cloud_storage(self, number_of_photos=5, folder='vk_profile_photos'):
         """
-        1. uploads photos to a yandex drive to the folder 'vk_profile_photos' in root directory.
-        2. creates a new file 'vk_profile_photos.json' with the list of parameters of photos.
-        :param number_of_photos: number of desired photos to download. default = 5
-        :param folder: name of the folder on yandex drive
+        1. Uploads photos to cloud storage to a folder in the root directory.
+        2. Creates a new file with the list of parameters of photos.
+        :param number_of_photos: number of desired photos to download
+        :param folder: name of the folder in cloud storage
         """
         self._get_profile_photos_data(number_of_photos)
         self._uploading(number_of_photos, folder)
 
     def upload_album_photos_to_cloud_storage(self, album_id, number_of_photos=5, folder='vk_album_photos'):
         """
-        1. uploads photos to a yandex drive to the folder 'vk_album_photos' in root directory.
-        2. creates a new file 'vk_album_photos.json' with the list of parameters of photos.
+        1. Uploads photos to cloud storage to a folder in the root directory.
+        2. Creates a new file with the list of parameters of photos.
         :param album_id: id of an album to download
-        :param number_of_photos: number of desired photos to download. default = 5
-        :param folder: name of the folder on yandex drive
+        :param number_of_photos: number of desired photos to download
+        :param folder: name of the folder in cloud storage
         """
         self._get_album_photos_data(album_id, number_of_photos)
         self._uploading(number_of_photos, folder)
@@ -57,7 +57,7 @@ class VkDownloader:
         }
 
         self.respond = requests.get('https://api.vk.com/method/photos.get',
-                                    params=param)  # getting the json for all profile photos
+                                    params=param)  # gets the .json file for the profile photos
 
     def _get_album_photos_data(self, album_id, number_of_photos):
         param = {
@@ -69,37 +69,40 @@ class VkDownloader:
             'v': '5.131'
         }
         self.respond = requests.get('https://api.vk.com/method/photos.get',
-                                    params=param)  # getting the json for all album photos
+                                    params=param)  # gets the .json file for the album photos
 
     def _same_likes_list(self):
         """
-        :return: list of likes that encounter more than once
+        :return: list of 'likes' that occur more than once
         """
         likes_list = Counter([likes['likes']['count'] for likes in self.respond.json()['response']['items']])
         return list(filter(lambda x: likes_list[x] > 1, likes_list))
 
     def _bar_number(self, number_of_photos):
-        if self.respond.json()['response']['count'] < number_of_photos:  # determining the length for the progress bar
+        """
+        Determines the length for the progress bar.
+        """
+        if self.respond.json()['response']['count'] < number_of_photos:
             return self.respond.json()['response']['count']
         else:
             return number_of_photos
 
     def _create_yandex_folder(self, path):
         """
-        Creates a folder on yandex drive in root folder
+        Creates the folder on yandex drive in the root folder.
         :param path: name of the folder
         """
         requests.put('https://cloud-api.yandex.net/v1/disk/resources',
                      headers={'Content-Type': 'application/json',
                               'Authorization': f'OAuth {self.ya_token}'},
-                     params={'path': f'/{path}'})  # creating the folder on yandex drive
+                     params={'path': f'/{path}'})  # creates the folder on yandex drive
         time.sleep(0.33)
 
     def _create_google_folder(self, path):
         """
-        Creates a folder on google drive in root folder
+        Creates the folder on google drive in the root folder.
         :param path: name of the folder
-        :return: folder_id - if of the folder on google drive,
+        :return: folder_id - id of the folder on google drive
                  folder_name - name of the folder on google drive
         """
         metadata = {
@@ -110,15 +113,15 @@ class VkDownloader:
         respond = requests.post('https://www.googleapis.com/drive/v3/files',
                                 headers={'Authorization': 'Bearer ' + self.google_token,
                                          'Content-Type': 'application/json'},
-                                data=json.dumps(metadata))  # creating the folder on google drive
+                                data=json.dumps(metadata))  # creates the folder on google drive
         time.sleep(0.33)
         return respond.json()['id'], respond.json()['name']
 
     def _uploading_to_yandex(self, same_likes, path, bar):
         """
-        Uploads photos to yandex drive
-        :param same_likes: list of likes that encounter more than once
-        :param path: folder_name on yandex drive where photos will  be uploaded
+        Uploads photos to yandex drive.
+        :param same_likes: list of likes that occur more than once
+        :param path: folder_name on yandex drive where photos will be uploaded
         :param bar: progress bar in terminal
         """
         self.data_file = {'photos': []}
@@ -128,7 +131,7 @@ class VkDownloader:
         for photo in self.respond.json()['response']['items']:
             url = photo['sizes'][-1]['url']
             size = photo['sizes'][-1]['type']
-            if photo['likes']['count'] in same_likes:  # checking if a date needs to be added to the name
+            if photo['likes']['count'] in same_likes:  # checks if a date needs to be added to the name
                 file_name = f"{photo['likes']['count']} - {time.strftime('%d.%b.%Y', time.gmtime(photo['date']))}.jpg"
             else:
                 file_name = f"{photo['likes']['count']}.jpg"
@@ -138,15 +141,15 @@ class VkDownloader:
             requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
                           headers={'Content-Type': 'application/json',
                                    'Authorization': f'OAuth {self.ya_token}'},
-                          params={'path': f'/{path}/{file_name}', 'url': url})  # uploading photos
+                          params={'path': f'/{path}/{file_name}', 'url': url})  # uploads photos
             bar.next()
             time.sleep(0.33)
 
     def _uploading_to_google(self, same_likes, path, bar):
         """
-        Uploads photos to google drive
-        :param same_likes: list of likes that encounter more than once
-        :param path: folder_id on yandex drive where photos will be uploaded
+        Uploads photos to google drive.
+        :param same_likes: list of likes that occur more than once
+        :param path: folder_id on google drive where photos will be uploaded
         :param bar: progress bar in terminal
         """
         self.data_file = {'photos': []}
@@ -156,7 +159,7 @@ class VkDownloader:
         for photo in self.respond.json()['response']['items']:
             url = photo['sizes'][-1]['url']
             size = photo['sizes'][-1]['type']
-            if photo['likes']['count'] in same_likes:  # checking if a date needs to be added to the name
+            if photo['likes']['count'] in same_likes:  # checks if a date needs to be added to the name
                 file_name = f"{photo['likes']['count']} - {time.strftime('%d.%b.%Y', time.gmtime(photo['date']))}.jpg"
             else:
                 file_name = f"{photo['likes']['count']}.jpg"
@@ -169,15 +172,15 @@ class VkDownloader:
             requests.post('https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart',
                           headers={"Authorization": "Bearer " + self.google_token},
                           files={"data": ("metadata", json.dumps(param), "application/json; charset=UTF-8"),
-                                 "file": requests.get(url).content})  # uploading photos
+                                 "file": requests.get(url).content})  # uploads photos
 
             bar.next()
             time.sleep(0.33)
 
     def _uploading(self, number_of_photos, folder_name):
         """
-        1. Chooses if the program should upload pictures to yandex or google drive
-        2. Creates a .json data file with pictures information
+        1. Chooses if the program should upload pictures to yandex or google drive.
+        2. Creates a .json data file with pictures' information.
         """
 
         same_likes = self._same_likes_list()
@@ -187,7 +190,7 @@ class VkDownloader:
         if self.ya_token != 'None' != self.google_token:
             bar_number *= 2
 
-        bar = IncrementalBar('Photos', max=bar_number)  # setting the progress bar
+        bar = IncrementalBar('Photos_uploading...', max=bar_number)  # sets the progress bar
 
         if self.ya_token != 'None':
             self._uploading_to_yandex(same_likes, folder_name, bar)
@@ -195,7 +198,7 @@ class VkDownloader:
             self._uploading_to_google(same_likes, folder_name, bar)
 
         with open(f'{folder_name}.json', 'w') as new_file:
-            json.dump(self.data_file, new_file, ensure_ascii=False, indent=4)  # creating the json data
+            json.dump(self.data_file, new_file, ensure_ascii=False, indent=4)  # creates the .json data
 
         bar.finish()
         print('Success!')
@@ -214,10 +217,11 @@ class OkDownloader(VkDownloader):
         self.profile_id = profile_id
         self.data_file = {'photos': []}  # future json file
         with open('ok_token.txt', 'r') as token:
-            self.ok_app_id = str(token.readline()).strip()
-            self.ok_app_key = str(token.readline()).strip()
-            self.ok_access_token = str(token.readline()).strip()
-            self.ok_secret_session_key = str(token.readline()).strip()
+            # These next 4 lines should be in exact order in the 'ok_token.txt' !
+            self.ok_app_id = str(token.readline()).strip()              # Application ID
+            self.ok_app_key = str(token.readline()).strip()             # Application public key
+            self.ok_access_token = str(token.readline()).strip()        # Access token
+            self.ok_secret_session_key = str(token.readline()).strip()  # Secret session key
         self.respond = None
         self.data_file = {'photos': []}
 
@@ -268,15 +272,18 @@ class OkDownloader(VkDownloader):
         self.respond = requests.get('https://api.ok.ru/api/photos/getPhotos', params=param)
 
     def _bar_number(self, number_of_photos):
-        if len(self.respond.json()['photos']) < number_of_photos:  # determining the length for the progress bar
+        """
+        Determines the length for the progress bar.
+        """
+        if len(self.respond.json()['photos']) < number_of_photos:
             return len(self.respond.json()['photos'])
         else:
             return number_of_photos
 
     def _uploading_to_yandex(self, path, bar):
         """
-        Uploads photos to yandex drive
-        :param path: folder_name on yandex drive where photos will  be uploaded
+        Uploads photos to yandex drive.
+        :param path: folder_name on yandex drive where photos will be uploaded
         :param bar: progress bar in terminal
         """
         self.data_file = {'photos': []}
@@ -293,14 +300,14 @@ class OkDownloader(VkDownloader):
             requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
                           headers={'Content-Type': 'application/json',
                                    'Authorization': f'OAuth {self.ya_token}'},
-                          params={'path': f'/{path}/{file_name}', 'url': url})  # uploading photos
+                          params={'path': f'/{path}/{file_name}', 'url': url})  # uploads photos
             bar.next()
             time.sleep(0.33)
 
     def _uploading_to_google(self, path, bar):
         """
-        Uploads photos to google drive
-        :param path: folder_id on yandex drive where photos will be uploaded
+        Uploads photos to google drive.
+        :param path: folder_id on google drive where photos will be uploaded
         :param bar: progress bar in terminal
         """
         self.data_file = {'photos': []}
@@ -320,15 +327,15 @@ class OkDownloader(VkDownloader):
             requests.post('https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart',
                           headers={"Authorization": "Bearer " + self.google_token},
                           files={"data": ("metadata", json.dumps(param), "application/json; charset=UTF-8"),
-                                 "file": requests.get(url).content})  # uploading photos
+                                 "file": requests.get(url).content})  # uploads photos
 
             bar.next()
             time.sleep(0.33)
 
     def _uploading(self, number_of_photos, folder_name):
         """
-        1. Chooses if the program should upload pictures to yandex or google drive
-        2. Creates a .json data file with pictures information
+        1. Chooses if the program should upload pictures to yandex or google drive.
+        2. Creates a .json data file with pictures' information.
         """
 
         bar_number = self._bar_number(number_of_photos)
@@ -336,7 +343,7 @@ class OkDownloader(VkDownloader):
         if self.ya_token != 'None' != self.google_token:
             bar_number *= 2
 
-        bar = IncrementalBar('Photos', max=bar_number)  # setting the progress bar
+        bar = IncrementalBar('Photos_uploading...', max=bar_number)  # sets the progress bar
 
         if self.ya_token != 'None':
             self._uploading_to_yandex(folder_name, bar)
@@ -344,19 +351,25 @@ class OkDownloader(VkDownloader):
             self._uploading_to_google(folder_name, bar)
 
         with open(f'{folder_name}.json', 'w') as new_file:
-            json.dump(self.data_file, new_file, ensure_ascii=False, indent=4)  # creating the json data
+            json.dump(self.data_file, new_file, ensure_ascii=False, indent=4)  # creates the .json data
 
         bar.finish()
         print('Success!')
 
 
 if __name__ == '__main__':
-    download = VkDownloader('profile_id', ya_token='', google_token='')
+    vk_id = int(input('input id'))
+    download = VkDownloader(vk_id, ya_token='', google_token='')
 
     download.upload_profile_photos_to_cloud_storage(5)
-    download.upload_album_photos_to_cloud_storage('album_id')
 
-    download = OkDownloader('profile_id', ya_token='', google_token='')
+    vk_album_id = int(input('input album_id'))
+    download.upload_album_photos_to_cloud_storage(vk_album_id)
+
+    ok_id = int(input('input id'))
+    download = OkDownloader(ok_id, ya_token='', google_token='')
 
     download.upload_profile_photos_to_cloud_storage(5)
-    download.upload_album_photos_to_cloud_storage('album_id')
+
+    ok_album_id = int(input('input album_id'))
+    download.upload_album_photos_to_cloud_storage(ok_album_id)
